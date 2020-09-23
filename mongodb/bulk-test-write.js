8d815f47f1ea3a7cabea80ccfc30c40cc1c64751
Puneet Singh <puneet.mir@gmail.com>
2020-09-23 06:56:12 +0530

    high mem example

--- mongodb/bulk-test-write.js
+++ mongodb/bulk-test-write.js
@@ -12,3 +12,8 @@ for (var i = 0; i < 1000000; i++) {x.push({insertOne:{"document":{"title" : "The
 // You cannot re-execute this alone, as all rows have _id's set. You need to 
 // execute both of all of these statements to load more data
 db.testone.bulkWrite(x);
+
+
+// better example which can create memory pressure on pod
+for (var i = 0; i < 10000000; i++) {x.push({insertOne:{"document":{"title" : "The Arrival of a Train","description":"Just some test data to load so that we can test the large size 
+data load for mongodb, plus keep appending more bytes with no purpose of this info","meta":{"labels":["cyka","blyat"]}}}})}
